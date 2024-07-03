007e479a42b95d0cac82d615ab2065f8e5397236
karmab <karimboumedhel@gmail.com>
2024-07-03 15:01:33 +0200

    Remove dns dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-dnf -y install --assumeyes centos-release-ceph-reef
+dnf -y install --assumeyes centos-release-ceph-{{ version }}
 dnf -y install --assumeyes cephadm
 
 mkdir -p /etc/ceph
@@ -6,7 +6,9 @@ mon_ip=$(hostname -I)
 cephadm bootstrap --mon-ip $mon_ip --allow-fqdn-hostname --initial-dashboard-password {{ admin_password }}
 fsid=$(cat /etc/ceph/ceph.conf | grep fsid | awk '{ print $3}')
 {% for number in range(1, nodes) %}
-  ssh-copy-id -f -i /etc/ceph/ceph.pub -o StrictHostKeyChecking=no root@{{ prefix }}-node-0{{ number }}
-  cephadm shell --fsid $fsid -c /etc/ceph/ceph.conf -k /etc/ceph/ceph.client.admin.keyring ceph orch host add {{ prefix }}-node-0{{ number }}.{{domain}}
+  {% set ip = '{0}-node-0{1}'.format(plan, number)|kcli_info('ip', wait=True) %}
+  echo {{ ip }} {{ plan }}-node-0{{ number }}.{{domain}} >> /etc/hosts
+  ssh-copy-id -f -i /etc/ceph/ceph.pub -o StrictHostKeyChecking=no root@{{ plan }}-node-0{{ number }}.{{domain}}
+  cephadm shell --fsid $fsid -c /etc/ceph/ceph.conf -k /etc/ceph/ceph.client.admin.keyring ceph orch host add {{ plan }}-node-0{{ number }}.{{domain}} {{ ip }}
 {% endfor %}
 cephadm shell --fsid $fsid -c /etc/ceph/ceph.conf -k /etc/ceph/ceph.client.admin.keyring ceph orch apply osd --all-available-devices
